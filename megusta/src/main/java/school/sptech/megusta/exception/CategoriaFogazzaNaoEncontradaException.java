122963a533df783753e999f6a089a9f236f62915
Munir004 <munir.hamze@sptech.school>
2026-04-14 17:41:06 -0300

    Criação do esqueleto de Categoria Fogazza

---
+++ megusta/src/main/java/school/sptech/megusta/exception/CategoriaFogazzaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package school.sptech.megusta.exception;
+
+public class CategoriaFogazzaNaoEncontradaException extends RuntimeException {
+    public CategoriaFogazzaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
