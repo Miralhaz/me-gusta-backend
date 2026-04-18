23dd6ffa2875148302dee54a9aac2458a7ffc2c8
enrico <enrico.faraldo@sptech.school>
2026-04-18 17:22:09 -0300

    implementando a autenticação com o JWT

---
+++ megusta/src/main/java/school/sptech/megusta/security/SecurityFilter.java
@@ -0,0 +1,54 @@
+package school.sptech.megusta.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import school.sptech.megusta.exception.TokenInvalidoException;
+import school.sptech.megusta.model.Usuario;
+import school.sptech.megusta.repository.UsuarioRepository;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+
+        if (tokenJWT != null){
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails usuario = usuarioRepository.findByNome(subject);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(usuario,
+                    null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
