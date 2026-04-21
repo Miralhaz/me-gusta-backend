d26284139516ab6ac277b35b2224027abbc08f6f
Miralhaz <lucas.silva051@sptech.school>
2026-04-21 00:21:25 -0300

    feat: Foreign Key de Status em Insumo

--- megusta/src/main/java/school/sptech/megusta/service/InsumoService.java
+++ megusta/src/main/java/school/sptech/megusta/service/InsumoService.java
@@ -3,12 +3,15 @@ package school.sptech.megusta.service;
 import org.springframework.stereotype.Service;
 import school.sptech.megusta.exception.CategoriaInsumoNaoEncontradaException;
 import school.sptech.megusta.exception.InsumoNaoEncontradoException;
+import school.sptech.megusta.exception.StatusNaoEncontradoException;
 import school.sptech.megusta.exception.UnidadeMedidaNaoEncontradaException;
 import school.sptech.megusta.model.CategoriaInsumo;
 import school.sptech.megusta.model.Insumo;
+import school.sptech.megusta.model.TipoStatus;
 import school.sptech.megusta.model.UnidadeMedida;
 import school.sptech.megusta.repository.CategoriaInsumoRepository;
 import school.sptech.megusta.repository.InsumoRepository;
+import school.sptech.megusta.repository.TipoStatusRepository;
 import school.sptech.megusta.repository.UnidadeMedidaRepository;
 
 import java.util.List;
@@ -19,11 +22,13 @@ public class InsumoService {
     private final InsumoRepository insumoRepository;
     private final CategoriaInsumoRepository categoriaInsumoRepository;
     private final UnidadeMedidaRepository unidadeMedidaRepository;
+    private final TipoStatusRepository tipoStatusRepository;
 
-    public InsumoService(CategoriaInsumoRepository categoriaInsumoRepository, InsumoRepository insumoRepository, UnidadeMedidaRepository unidadeMedidaRepository) {
-        this.categoriaInsumoRepository = categoriaInsumoRepository;
+    public InsumoService(InsumoRepository insumoRepository, CategoriaInsumoRepository categoriaInsumoRepository, UnidadeMedidaRepository unidadeMedidaRepository, TipoStatusRepository tipoStatusRepository) {
         this.insumoRepository = insumoRepository;
+        this.categoriaInsumoRepository = categoriaInsumoRepository;
         this.unidadeMedidaRepository = unidadeMedidaRepository;
+        this.tipoStatusRepository = tipoStatusRepository;
     }
 
     public List<Insumo> listar(){
@@ -42,8 +47,12 @@ public class InsumoService {
         UnidadeMedida unidadeMedida = unidadeMedidaRepository.findById(insumo.getUnidadeMedida().getId())
                 .orElseThrow(() -> new UnidadeMedidaNaoEncontradaException("Unidade de medida não encontrada."));
 
+        TipoStatus tipoStatus = tipoStatusRepository.findById(insumo.getTipoStatus().getId())
+                .orElseThrow(() -> new StatusNaoEncontradoException("Status não encontrada."));
+
         insumo.setCategoriaInsumo(categoriaInsumo);
         insumo.setUnidadeMedida(unidadeMedida);
+        insumo.setTipoStatus(tipoStatus);
         return insumoRepository.save(insumo);
     }
 
@@ -58,9 +67,13 @@ public class InsumoService {
         UnidadeMedida unidadeMedida = unidadeMedidaRepository.findById(insumo.getUnidadeMedida().getId())
                 .orElseThrow(() -> new UnidadeMedidaNaoEncontradaException("Unidade de medida não encontrada."));
 
+        TipoStatus tipoStatus = tipoStatusRepository.findById(insumo.getTipoStatus().getId())
+                .orElseThrow(() -> new StatusNaoEncontradoException("Status não encontrado."));
+
         insumoExistente.setId(id);
         insumoExistente.setCategoriaInsumo(categoriaInsumo);
         insumoExistente.setUnidadeMedida(unidadeMedida);
+        insumoExistente.setTipoStatus(tipoStatus);
         return insumoRepository.save(insumo);
     }
 }
