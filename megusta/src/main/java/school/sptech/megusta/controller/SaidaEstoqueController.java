4647c31b7da0af68ddd44ff2e7e52afdc60e1fb4
Miralhaz <lucas.silva051@sptech.school>
2026-06-05 18:58:24 -0300

    fix: Ajuste no Swagger

--- megusta/src/main/java/school/sptech/megusta/controller/SaidaEstoqueController.java
+++ megusta/src/main/java/school/sptech/megusta/controller/SaidaEstoqueController.java
@@ -1,5 +1,6 @@
 package school.sptech.megusta.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/saidas-estoque")
+@Tag(name = "11. Saídas de Estoque", description = "Gerenciamento de saídas de produtos do estoque")
 public class SaidaEstoqueController {
 
     private final SaidaEstoqueService service;
--- megusta/src/main/java/school/sptech/megusta/controller/VendasController.java
+++ megusta/src/main/java/school/sptech/megusta/controller/VendasController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/vendas")
 @RequiredArgsConstructor
+@Tag(name = "12. Vendas", description = "Gerenciamento de vendas")
 public class VendasController {
 
     private final VendasService vendasService;
