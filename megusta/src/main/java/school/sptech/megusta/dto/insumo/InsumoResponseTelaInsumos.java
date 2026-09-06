a0a5d4909934e15893bb98197bcd7f2a0970af44
Miralhaz <lucas.silva051@sptech.school>
2026-09-06 20:13:19 -0300

    fix(insumos): adicionando nova DTO para tela insumos (não atrapalhar lógica da tela Estoque)

---
+++ megusta/src/main/java/school/sptech/megusta/dto/insumo/InsumoResponseTelaInsumos.java
@@ -0,0 +1,57 @@
+package school.sptech.megusta.dto.insumo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class InsumoResponseTelaInsumos {
+
+    @Schema(example = "1")
+    private Integer id;
+
+    @Schema(example = "Queijo Mussarela")
+    private String nome;
+
+    @Schema(example = "QM-001")
+    private String codigoInsumo;
+
+    @Schema(example = "5.0")
+    private Double estoqueMinimo;
+
+    @Schema(example = "20.0")
+    private Double quantidadeAtual;
+
+    @Schema(example = "true")
+    private boolean ativo;
+
+    @Schema(example = "2026-08-26")
+    private LocalDate dtCadastro;
+
+    private InsumoResponseTelaInsumos.InsumoCategoria insumoCategoria;
+    private InsumoResponseTelaInsumos.UnidadeInsumo unidadeInsumo;
+
+    @Getter
+    @Setter
+    public static class InsumoCategoria {
+        @Schema(example = "1")
+        private Integer id;
+
+        @Schema(example = "Laticínios")
+        private String nome;
+    }
+
+    @Getter
+    @Setter
+    public static class UnidadeInsumo {
+        @Schema(example = "1")
+        private Integer id;
+
+        @Schema(example = "kg")
+        private String unidade;
+    }
+}
